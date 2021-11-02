843c1455d8b37a7f2bcd1614a45c7d07ead31ae8
Yurii Hrecheniuk <yuriy@hrecheni.uk>
2021-11-02 17:08:48 +0200

    Get one post by author done

--- src/routes/authors.js
+++ src/routes/authors.js
@@ -33,12 +33,29 @@ router.get('/', (req, res) => {
   res.json(authors);
 })
 
-router.get('/:id', (req, res, next) => {
-  const author = authors.find(author => author.id == req.params.id);
+router.param('id', (req, res, next, id) => {
+  const author = authors.find(author => author.id == id);
 
-  author ?
-    res.json(author) :
+  if (!author) {
     next(createError(500, 'There is no such author.'));
+    return;
+  }
+
+  req.author = author;
+  next();
+})
+
+router.get('/:id/posts', (req, res, next) => {
+  res.json(req.author.posts);
+})
+
+// todo: get one post by author
+router.get('/:id/posts/:postId', (req, res, next) => {
+  const post = req.author.posts.find(post => post.id == req.params.postId);
+
+  post ?
+    res.json(post) :
+    next(createError(500, `The user has only ${req.author.posts.length} posts.`));
 })
 
 module.exports = router;
@@ -47,5 +64,3 @@ module.exports = router;
 // todo: add author
 // todo: remove author
 // todo: rename author
-// todo: get posts by author
-// todo: get one post by author
